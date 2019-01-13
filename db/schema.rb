f5ba43ba02ab46de57b1652af520f73c282a78c5
cuongtm95 <stmanhcuongtran@gmail.com>
2019-01-13 16:30:41 +0100

    destroy 2 mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_181021) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "people", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.integer "password"
-    t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
