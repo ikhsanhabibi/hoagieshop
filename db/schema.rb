6e105cb1e0469aebbaf3a795e3e92a28b42cfb69
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-01-21 22:58:27 +0100

    mvc Order & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_200435) do
+ActiveRecord::Schema.define(version: 2019_01_21_215755) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "breadtype"
+    t.string "ingredient"
+    t.string "vegetable"
+    t.string "condiment"
+    t.decimal "price"
+    t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
