fb76ad75c8aad6c1cc0a52a68d257aab785c5446
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-01-29 21:26:56 +0100

    show the total price

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -61,6 +61,7 @@ class OrdersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   before_save do
     self.vegetable.gsub!(/[\[\]\"]/, "") if attribute_present?("vegetable")
   end
+
 end
