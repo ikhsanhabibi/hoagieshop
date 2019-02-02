7fe2ff19ae244c953754bc1d70a43ddffb0262a6
cuongtm95 <stmanhcuongtran@gmail.com>
2019-02-02 17:22:19 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'GLUTEN-FREE-BREAD', vegan: false, desc: "semi-skimmed milk, free-range eggs, white wine vinegar, gluten-free brown bread flour, sea salt, golden caster sugar, sachet dried yeast, olive oil")
+Product.create(name: 'CUCUMBERS', vegan: true)
+Product.create(name: 'CEASER SAUCE', vegan: false, desc: 'soybean oil, water, parmesan cheese, vinegar, eggs, sugar, salt, lemon juice, anchovy paste, garlic, onion, added to protect flavor')
+Product.create(name: 'BACON SLICES', vegan: false, desc: 'Pork, water, salt, sugar, natural smoke flavor, sodium phosphates, sodium nitrite')
